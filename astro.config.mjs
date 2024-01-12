e0baf94151b94e9ad41ff3437cb4184c72aeba53
Marc Wahlen <marc.wahlen@web.de>
2024-01-12 22:38:50 +0100

    update readme.md and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import typography from '@tailwindcss/typography'; 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],  
   output: "server",
   adapter: cloudflare(),
   prefetch: {
